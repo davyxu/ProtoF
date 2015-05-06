7a13ade47d44fc9b549cfe39468e2264ac5df225
xubo <sunicdavy@sina.com>
2015-05-06 18:44:28 +0800

    添加命令行处理

--- Source/Compiler/Program.cs
+++ Source/Compiler/Program.cs
@@ -3,13 +3,48 @@ using System.IO;
 using ProtoF.AST;
 using ProtoF.Parser;
 using ProtoF.Printer;
+using CommandLine;
+using CommandLine.Text;
+using System;
 
 namespace ProtoF
 {
+    class CommandOptions
+    {
+        [Option('m', "method", Required = true, HelpText = "method to operate")]
+        public string Method { get; set; }
+
+        [Option('i',"inputfile", Required=true, HelpText="input file name")]
+        public string InputFile { get; set; }
+
+        [Option('o', "outputfile", Required = true, HelpText = "output file name")]
+        public string OutputFile { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            return HelpText.AutoBuild(this,
+              (HelpText current) => {
+
+
+                  HelpText.DefaultParsingErrorsHandler(this, current);
+              });
+        }
+    }
+
     class Program
     {
+        private static readonly HeadingInfo HeadingInfo = new HeadingInfo("ProtoF", "1.0");
+
         static void Main(string[] args)
         {
+            var options = new CommandOptions();
+            if (!CommandLine.Parser.Default.ParseArguments(args, options))
+            {
+                Environment.Exit(-1);
+                return;
+            }
+
             var parser = new SchemaParser();
 
             var inputFile = "../../../proto/protof.protof";
